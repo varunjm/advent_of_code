f84038920bf0377380833808425275a0957812bd
varun.jm26@gmail.com <vjayathirtha@nvidia.com>
2019-12-03 15:12:35 -0800

    Use tuple (x, y) in place of str((x, y)). Improves speed by a lot

--- 2019/3a.py
+++ 2019/3a.py
@@ -1,7 +1,7 @@
 import sys
 
 curr = (0, 0)
-points = {"0,0"}
+points = {(0,0)}
 min_dist = 9223372036854775807
 
 def UpdatePoints(move):
@@ -9,18 +9,19 @@ def UpdatePoints(move):
   global points
   dist = int(move[1:])
   if move[0] == 'R':
-    points.update([ (str(curr[0]+i)+","+str(curr[1])) for i in range(1, dist+1)])
+    points.update([ ((curr[0]+i),(curr[1])) for i in range(1, dist+1)])
     curr = (curr[0]+dist, curr[1])
   elif move[0] == 'L':
-    points.update([ (str(curr[0]-i)+","+str(curr[1])) for i in range(1, dist+1)])
+    points.update([ ((curr[0]-i),(curr[1])) for i in range(1, dist+1)])
     curr = (curr[0]-dist, curr[1])
   elif move[0] == 'U':
-    points.update([ (str(curr[0])+","+str(curr[1]+i)) for i in range(1, dist+1)])
+    points.update([ ((curr[0]), (curr[1]+i)) for i in range(1, dist+1)])
     curr = (curr[0], curr[1]+dist)
   elif move[0] == 'D':
-    points.update([ (str(curr[0])+","+str(curr[1]-i)) for i in range(1, dist+1)])
+    points.update([ ((curr[0]), (curr[1]-i)) for i in range(1, dist+1)])
     curr = (curr[0], curr[1]-dist)
 
+# Identify intersections and find the intersection nearest(manhattan distance) to origin
 def IdentifyIntersections(move):
   global curr
   global points
@@ -29,43 +30,40 @@ def IdentifyIntersections(move):
   dist = int(move[1:])
   if move[0] == 'R':
     for i in range(1, dist+1):
-      xy = (str(curr[0]+i)+","+str(curr[1]))
+      xy = ((curr[0]+i),(curr[1]))
       if xy in points:
-        # print "Meeting at " + xy
         min_dist = min(min_dist, abs(curr[0]+i) + abs(curr[1]))
     curr = (curr[0]+dist, curr[1])
   elif move[0] == 'L':
     for i in range(1, dist+1):
-      xy = (str(curr[0]-i)+","+str(curr[1]))
+      xy = ((curr[0]-i),(curr[1]))
       if xy in points:
-        # print "Meeting at " + xy
         min_dist = min(min_dist, abs(curr[0]-i) + abs(curr[1]))
     curr = (curr[0]-dist, curr[1])
   elif move[0] == 'U':
     for i in range(1, dist+1):
-      xy = (str(curr[0])+","+str(curr[1]+i))
+      xy = ((curr[0]),(curr[1]+i))
       if xy in points:
-        # print "Meeting at " + xy
         min_dist = min(min_dist, abs(curr[0]) + abs(curr[1]+i))
     curr = (curr[0], curr[1]+dist)
   elif move[0] == 'D':
     for i in range(1, dist+1):
-      xy = (str(curr[0])+","+str(curr[1]-i))
+      xy = ((curr[0]),(curr[1]-i))
       if xy in points:
-        # print "Meeting at " + xy
         min_dist = min(min_dist, abs(curr[0]) + abs(curr[1]-i))
     curr = (curr[0], curr[1]-dist)
 
 f = open(sys.argv[1], "r")
-wires = f.readlines()
 
-for move in wires[0].split(","):
+#wire 1
+for move in f.readline().split(","):
   UpdatePoints(move)
 
-points.remove("0,0")
+points.remove((0,0))
 curr = (0, 0)
 
-for move in wires[1].split(","):
+#wire 2
+for move in f.readline().split(","):
   IdentifyIntersections(move)
 
 print(min_dist)
--- 2019/3b.py
+++ 2019/3b.py
@@ -6,9 +6,6 @@ points = [(0,0)]
 min_steps = 9223372036854775807
 steps2 = 0
 
-def getstr(xy):
-  return (str(xy[0])+","+str(xy[1]))
-
 def UpdatePoints(move):
   global curr
   global points
@@ -26,6 +23,7 @@ def UpdatePoints(move):
     points.extend([ (curr[0], curr[1]-i) for i in range(1, dist+1)])
     curr = (curr[0], curr[1]-dist)
 
+# Identify intersections and find smallest number of steps needed to reach an intersection
 def IdentifyIntersections(move):
   global curr
   global dist_to_points
@@ -33,32 +31,28 @@ def IdentifyIntersections(move):
   global min_steps
 
   dist = int(move[1:])
-  if move[0] == 'R':
+  if move[0] == 'R' :
     for i in range(1, dist+1):
-      xy = getstr((curr[0]+i, curr[1]))
+      xy = (curr[0]+i, curr[1])
       if xy in dist_to_points:
-        # print "Meeting at " + xy
         min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
     curr = (curr[0]+dist, curr[1])
   elif move[0] == 'L':
     for i in range(1, dist+1):
-      xy = getstr((curr[0]-i, curr[1]))
+      xy = (curr[0]-i, curr[1])
       if xy in dist_to_points:
-        # print "Meeting at " + xy
         min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
     curr = (curr[0]-dist, curr[1])
   elif move[0] == 'U':
     for i in range(1, dist+1):
-      xy = getstr((curr[0], curr[1]+i))
+      xy = (curr[0], curr[1]+i)
       if xy in dist_to_points:
-        # print "Meeting at " + xy
         min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
     curr = (curr[0], curr[1]+dist)
   elif move[0] == 'D':
     for i in range(1, dist+1):
-      xy = getstr((curr[0], curr[1]-i))
+      xy = (curr[0], curr[1]-i)
       if xy in dist_to_points:
-        # print "Meeting at " + xy
         min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
     curr = (curr[0], curr[1]-dist)
   steps2 = steps2 + dist
@@ -69,18 +63,12 @@ f = open(sys.argv[1], "r")
 for move in f.readline().split(","):
   UpdatePoints(move)
 
-used = set()
-# points = [x for x in points if x not in used and (used.add(x) or True)]
 for idx, point in enumerate(points):
-  if point not in used:
-    used.add(point)
-    xy = getstr(point)
-    dist_to_points[xy] = idx
+  if point not in dist_to_points:
+    dist_to_points[point] = idx
 
-del dist_to_points["0,0"]
+del dist_to_points[(0,0)]
 curr = (0, 0)
-
-# print(points)
 
 #wire 2
 for move in f.readline().split(","):
