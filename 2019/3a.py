80da2e341ef6d784efbb9581f7f6c9f3f1f44ce1
varun.jm26@gmail.com <vjayathirtha@nvidia.com>
2019-12-03 16:48:54 -0800

    Slightly slower albeit less repetitive and shorter code.

--- 2019/3a.py
+++ 2019/3a.py
@@ -3,23 +3,18 @@ import sys
 curr = (0, 0)
 points = {(0,0)}
 min_dist = 9223372036854775807
+delta = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
+
+def manhattan_dist(xy):
+  return abs(xy[0]) + abs(xy[1])
 
 def UpdatePoints(move):
   global curr
   global points
+  move_vector = delta[move[0]]
   dist = int(move[1:])
-  if move[0] == 'R':
-    points.update([ ((curr[0]+i),(curr[1])) for i in range(1, dist+1)])
-    curr = (curr[0]+dist, curr[1])
-  elif move[0] == 'L':
-    points.update([ ((curr[0]-i),(curr[1])) for i in range(1, dist+1)])
-    curr = (curr[0]-dist, curr[1])
-  elif move[0] == 'U':
-    points.update([ ((curr[0]), (curr[1]+i)) for i in range(1, dist+1)])
-    curr = (curr[0], curr[1]+dist)
-  elif move[0] == 'D':
-    points.update([ ((curr[0]), (curr[1]-i)) for i in range(1, dist+1)])
-    curr = (curr[0], curr[1]-dist)
+  points.update([(curr[0]+(move_vector[0]*i), curr[1]+(move_vector[1]*i)) for i in range(1, dist+1)])
+  curr = (curr[0]+(move_vector[0]*dist), curr[1]+(move_vector[1]*dist))
 
 # Identify intersections and find the intersection nearest(manhattan distance) to origin
 def IdentifyIntersections(move):
@@ -27,43 +22,23 @@ def IdentifyIntersections(move):
   global points
   global min_dist
 
+  move_vector = delta[move[0]]
   dist = int(move[1:])
-  if move[0] == 'R':
-    for i in range(1, dist+1):
-      xy = ((curr[0]+i),(curr[1]))
-      if xy in points:
-        min_dist = min(min_dist, abs(curr[0]+i) + abs(curr[1]))
-    curr = (curr[0]+dist, curr[1])
-  elif move[0] == 'L':
-    for i in range(1, dist+1):
-      xy = ((curr[0]-i),(curr[1]))
-      if xy in points:
-        min_dist = min(min_dist, abs(curr[0]-i) + abs(curr[1]))
-    curr = (curr[0]-dist, curr[1])
-  elif move[0] == 'U':
-    for i in range(1, dist+1):
-      xy = ((curr[0]),(curr[1]+i))
-      if xy in points:
-        min_dist = min(min_dist, abs(curr[0]) + abs(curr[1]+i))
-    curr = (curr[0], curr[1]+dist)
-  elif move[0] == 'D':
-    for i in range(1, dist+1):
-      xy = ((curr[0]),(curr[1]-i))
-      if xy in points:
-        min_dist = min(min_dist, abs(curr[0]) + abs(curr[1]-i))
-    curr = (curr[0], curr[1]-dist)
+  for i in range(1, dist+1):
+    xy = (curr[0]+(move_vector[0]*i), curr[1]+(move_vector[1]*i))
+    if xy in points:
+      min_dist = min(min_dist, manhattan_dist(xy))
+  curr = xy
 
 f = open(sys.argv[1], "r")
 
 #wire 1
-for move in f.readline().split(","):
-  UpdatePoints(move)
+map(UpdatePoints, f.readline().split(","))
 
 points.remove((0,0))
 curr = (0, 0)
 
 #wire 2
-for move in f.readline().split(","):
-  IdentifyIntersections(move)
+map(IdentifyIntersections, f.readline().split(","))
 
 print(min_dist)
--- 2019/3b.py
+++ 2019/3b.py
@@ -4,24 +4,16 @@ dist_to_points = dict()
 curr = (0, 0)
 points = [(0,0)]
 min_steps = 9223372036854775807
+delta = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
 steps2 = 0
 
 def UpdatePoints(move):
   global curr
   global points
+  move_vector = delta[move[0]]
   dist = int(move[1:])
-  if move[0] == 'R':
-    points.extend([ (curr[0]+i, curr[1]) for i in range(1, dist+1)])
-    curr = (curr[0]+dist, curr[1])
-  elif move[0] == 'L':
-    points.extend([ (curr[0]-i, curr[1]) for i in range(1, dist+1)])
-    curr = (curr[0]-dist, curr[1])
-  elif move[0] == 'U':
-    points.extend([ (curr[0], curr[1]+i) for i in range(1, dist+1)])
-    curr = (curr[0], curr[1]+dist)
-  elif move[0] == 'D':
-    points.extend([ (curr[0], curr[1]-i) for i in range(1, dist+1)])
-    curr = (curr[0], curr[1]-dist)
+  points.extend([(curr[0]+(move_vector[0]*i), curr[1]+(move_vector[1]*i)) for i in range(1, dist+1)])
+  curr = points[-1]
 
 # Identify intersections and find smallest number of steps needed to reach an intersection
 def IdentifyIntersections(move):
@@ -30,38 +22,19 @@ def IdentifyIntersections(move):
   global steps2
   global min_steps
 
+  move_vector = delta[move[0]]
   dist = int(move[1:])
-  if move[0] == 'R' :
-    for i in range(1, dist+1):
-      xy = (curr[0]+i, curr[1])
-      if xy in dist_to_points:
-        min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
-    curr = (curr[0]+dist, curr[1])
-  elif move[0] == 'L':
-    for i in range(1, dist+1):
-      xy = (curr[0]-i, curr[1])
-      if xy in dist_to_points:
-        min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
-    curr = (curr[0]-dist, curr[1])
-  elif move[0] == 'U':
-    for i in range(1, dist+1):
-      xy = (curr[0], curr[1]+i)
-      if xy in dist_to_points:
-        min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
-    curr = (curr[0], curr[1]+dist)
-  elif move[0] == 'D':
-    for i in range(1, dist+1):
-      xy = (curr[0], curr[1]-i)
-      if xy in dist_to_points:
-        min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
-    curr = (curr[0], curr[1]-dist)
+  for i in range(1, dist+1):
+    xy = (curr[0]+(move_vector[0]*i), curr[1]+(move_vector[1]*i))
+    if xy in dist_to_points:
+      min_steps = min(min_steps, dist_to_points[xy] + steps2 + i)
+  curr = xy
   steps2 = steps2 + dist
 
 f = open(sys.argv[1], "r")
 
 #wire 1
-for move in f.readline().split(","):
-  UpdatePoints(move)
+map(UpdatePoints, f.readline().split(","))
 
 for idx, point in enumerate(points):
   if point not in dist_to_points:
@@ -71,7 +44,6 @@ del dist_to_points[(0,0)]
 curr = (0, 0)
 
 #wire 2
-for move in f.readline().split(","):
-  IdentifyIntersections(move)
+map(IdentifyIntersections, f.readline().split(","))
 
 print(min_steps)
