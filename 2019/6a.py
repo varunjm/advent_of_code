68c4960d8014ac37abb3652110376aeed1679dff
varun.jm26@gmail.com <vjayathirtha@nvidia.com>
2019-12-06 11:16:12 -0800

    Simplify code

--- 2019/6a.py
+++ 2019/6a.py
@@ -1,29 +1,19 @@
 import sys
 
-f = open(sys.argv[1], "r")
-
 orbit_map = dict()
 orbit_count = dict()
 
-for orbit in f.readlines():
-  brace = orbit.find(')')
-  orbitee = orbit[:brace]
-  orbiter = orbit[brace+1:-1]
-  if orbiter not in orbit_map:
-    orbit_map[orbiter] = orbitee
-    # print orbit_map
-  # else:
-  #   orbit_map[orbiter] = orbit_map[orbiter].extend(orbitee)
-  # print(orbitee, orbiter)
+with open(sys.argv[1], "r") as orbits:
+  for orbit in orbits:
+    orbitee, orbiter = orbit.strip().split(')')
+    if orbiter not in orbit_map:
+      orbit_map[orbiter] = orbitee
 
-for orbiter in orbit_map.keys():
+for orbiter in orbit_map:
   orbitee = orbit_map[orbiter]
   count = 0
   while orbitee:
-    count += 1
-    orbitee = orbit_map[orbitee] if orbitee in orbit_map.keys() else False
+    count, orbitee = (orbit_count[orbitee] + count + 1, None) if orbitee in orbit_count else (count + 1, orbit_map[orbitee]) if orbitee in orbit_map else (count + 1, None)
   orbit_count[orbiter] = count
 
-# print(orbit_map)
-# print(orbit_count)
 print(sum(orbit_count.values()))
--- 2019/6b.py
+++ 2019/6b.py
@@ -1,27 +1,22 @@
 import sys
 from collections import deque
 
-f = open(sys.argv[1], "r")
 orbital_graph = dict()
 
-for orbit in f.readlines():
-  brace = orbit.find(')')
-  orbitee = orbit[:brace]
-  orbiter = orbit[brace+1:-1]
-  if orbiter == 'SAN':
-    target = orbitee
-  if orbiter == 'YOU':
-    source = orbitee 
-  if orbiter not in orbital_graph:
-    orbital_graph[orbiter] = []
-  if orbitee not in orbital_graph:
-    orbital_graph[orbitee] = []
-  orbital_graph[orbitee].append(orbiter)
-  orbital_graph[orbiter].append(orbitee)
+# Build graph of orbitals
+with open(sys.argv[1], "r") as orbits:
+  for orbit in orbits:
+    orbitee, orbiter = orbit.strip().split(')')
+    if orbiter == 'SAN':
+      target = orbitee 
+    if orbiter == 'YOU':
+      source = orbitee 
+    orbital_graph[orbiter] = [] if orbiter not in orbital_graph else orbital_graph[orbiter]
+    orbital_graph[orbitee] = [] if orbitee not in orbital_graph else orbital_graph[orbitee]
+    orbital_graph[orbitee].append(orbiter)
+    orbital_graph[orbiter].append(orbitee)
 
-# print(orbital_graph)
-print(target)
-
+# Breadth first search
 visited = set()
 q = deque() 
 q.append((source, 0))
@@ -31,10 +26,7 @@ while True:
   if current == target:
     break
   if current not in visited:
-    print (current, depth)
     visited.add(current)
-    # print (current)
-    # map(q.append, orbital_graph[current])
     for orbital in orbital_graph[current]:
       q.append((orbital, depth+1))
 
