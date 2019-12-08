653ea32253bf68a4b0aa19fe86f84357113992aa
varun.jm26@gmail.com <vjayathirtha@nvidia.com>
2019-12-08 02:34:39 -0800

    Refactor Intcodecomputer to a separate file. Reuse between 7a and 7b

--- 2019/7b.py
+++ 2019/7b.py
@@ -1,86 +1,8 @@
 import sys
 import itertools
+from IntcodeComputer import IntcodeComputer
 
 f = open(sys.argv[1], "r")
-
-class IntcodeComputer:
-  four_op_instr_codes = [1, 2, 7 ,8]
-  two_op_instr_codes = [5, 6]
-
-  def __init__(self, instrs):
-    #list of instructions
-    self.instrs = instrs
-    #program counter
-    self.PC = 0
-    self.phase = None
-    self.signal = None
-  
-  def set_phase(self, ph):
-    self.phase = ph
-
-  def set_signal(self, input):
-    self.signal = input
-
-  def print_computer (self):
-    print ('instr', self.instrs)
-    print ('PC', self.PC)
-    print ('Signal', self.signal)
-    print ('Phase', self.phase)
-
-  def fetch_2_params(self):
-    param_mode = str(self.instrs[self.PC]/100)[::-1].ljust(2, '0')
-    param1 = self.instrs[self.PC+1] if param_mode[0] == '1' else self.instrs[self.instrs[self.PC+1]]
-    param2 = self.instrs[self.PC+2] if param_mode[1] == '1' else self.instrs[self.instrs[self.PC+2]]
-    return param1, param2
-
-  def fetch_1_param(self):
-    param_mode = str(self.instrs[self.PC]/100)[::-1].ljust(2, '0')
-    param1 = self.instrs[self.PC+1] if param_mode[0] == '1' else self.instrs[self.instrs[self.PC+1]]
-    return param1
-
-  def exec_next_instr(self):
-    opcode = self.instrs[self.PC]%100
-    if opcode in self.four_op_instr_codes:
-      param1, param2 = self.fetch_2_params()
-      if opcode == 1:
-        self.instrs[self.instrs[self.PC+3]] = param1 + param2
-      elif opcode == 2:
-        self.instrs[self.instrs[self.PC+3]] = param1 * param2
-      elif opcode == 7:
-        self.instrs[self.instrs[self.PC+3]] = 1 if param1 < param2 else 0
-      elif opcode == 8:
-        self.instrs[self.instrs[self.PC+3]] = 1 if param1 == param2 else 0
-      self.PC += 4
-    elif opcode == 3:
-      if self.phase != None:
-        self.instrs[self.instrs[self.PC+1]] = self.phase
-        self.phase = None
-      else :
-        self.instrs[self.instrs[self.PC+1]] = self.signal
-      self.PC += 2
-    elif opcode == 4:
-      return_value = self.fetch_1_param()
-      self.PC += 2
-      return (opcode, return_value)
-    elif opcode in self.two_op_instr_codes:
-      param1, param2 = self.fetch_2_params()
-      if (param1 != 0 and opcode == 5) or (param1 == 0 and opcode == 6) :
-        self.PC = param2
-      else:
-        self.PC += 3
-    elif opcode == 99:
-      return (opcode, None)
-    else:
-      print ("ERROR")
-      sys.exit()
-    return (opcode, None)
-
-  def run(self):
-    result = None
-    opcode = -1
-    while not result and opcode != 99:
-      opcode, result = self.exec_next_instr()
-    return result
 
 program = [int(element) for element in f.readline().split(",")]
 
@@ -93,19 +15,15 @@ for phases in itertools.permutations([5,6,7,8,9]):
     Amplifier[idx].set_phase(phase)
 
   idx = 0
-  output_signal = 0
   thrust = 0
-  while True:
-    Amplifier[idx].set_signal(output_signal)
-    output_signal = Amplifier[idx].run()
-    if output_signal == None:
-      break
+  output_signal = 0
+  while output_signal != None:
     thrust = output_signal
+    output_signal = Amplifier[idx].run()
     idx = (idx + 1) % 5
+    Amplifier[idx].set_signal(output_signal)
 
-  if max_thrust < thrust:
-    max_thrust = thrust
-    max_thrust_phases = phases
+  max_thrust, max_thrust_phases = (thrust, phases) if max_thrust < thrust else (max_thrust, max_thrust_phases)
 
 print(max_thrust_phases)
 print(max_thrust)
---
+++ 2019/IntcodeComputer.py
@@ -0,0 +1,72 @@
+class IntcodeComputer:
+  three_opnds_instr_codes = [1, 2, 7 ,8]
+  two_opnds_instr_codes = [5, 6]
+  operations = {
+    1: lambda p1, p2: p1 + p2,
+    2: lambda p1, p2: p1 * p2,
+    5: lambda p1, p2, pc: p2 if p1 != 0 else pc + 3,
+    6: lambda p1, p2, pc: p2 if p1 == 0 else pc + 3,
+    7: lambda p1, p2: 1 if p1 < p2 else 0,
+    8: lambda p1, p2: 1 if p1 == p2 else 0,
+  }
+
+  def __init__(self, instrs):
+    self.instrs = instrs
+    self.PC = 0
+    self.phase = None
+    self.signal = 0
+  
+  def set_phase(self, ph):
+    self.phase = ph
+
+  def set_signal(self, input):
+    self.signal = input
+
+  def fetch_1_opnd(self):
+    param_mode = str(self.instrs[self.PC]/100)[::-1].ljust(2, '0')
+    param1 = self.instrs[self.PC+1] if param_mode[0] == '1' else self.instrs[self.instrs[self.PC+1]]
+    return param1
+
+  def fetch_2_opnds(self):
+    param_mode = str(self.instrs[self.PC]/100)[::-1].ljust(2, '0')
+    param1 = self.instrs[self.PC+1] if param_mode[0] == '1' else self.instrs[self.instrs[self.PC+1]]
+    param2 = self.instrs[self.PC+2] if param_mode[1] == '1' else self.instrs[self.instrs[self.PC+2]]
+    return param1, param2
+
+  def fetch_3_opnds(self):
+    param1, param2 = self.fetch_2_opnds()
+    dest = self.instrs[self.PC+3]
+    return param1, param2, dest
+
+  def exec_next_instr(self):
+    opcode = self.instrs[self.PC]%100
+    if opcode in self.three_opnds_instr_codes:
+      param1, param2, dest = self.fetch_3_opnds()
+      self.instrs[dest] = self.operations[opcode](param1, param2)
+      self.PC += 4
+
+    elif opcode == 3:
+      self.instrs[self.instrs[self.PC+1]] = self.phase if self.phase != None else self.signal
+      self.phase = None
+      self.PC += 2
+
+    elif opcode == 4:
+      return_value = self.fetch_1_opnd()
+      self.PC += 2
+      return (opcode, return_value)
+
+    elif opcode in self.two_opnds_instr_codes:
+      param1, param2 = self.fetch_2_opnds()
+      self.PC = self.operations[opcode](param1, param2, self.PC)
+
+    elif opcode == 99:
+      return (opcode, None)
+
+    return (opcode, None)
+
+  def run(self):
+    result = None
+    opcode = -1
+    while not result and opcode != 99:
+      opcode, result = self.exec_next_instr()
+    return result
